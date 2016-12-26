8dfdf16f6bc935a2960f8683fbfa3fbe87f609a5
oğuz korkut <oguzkrkt@gmail.com>
2016-12-26 07:15:00 +0200

    müşteri bilgileri sayfasında html içinde tanımlı model düzenlendi

--- IKAppWeb/src/main/webapp/public/js/controller/userDetailController.js
+++ IKAppWeb/src/main/webapp/public/js/controller/userDetailController.js
@@ -7,6 +7,9 @@ app.controller('userDetailController',function($scope, $uibModal,ikModalService,
 	var promise = userService.getUserDetailById(user.id)
 	
 	promise.then(function success(data) {
+		if (data.profileDto.birthDate || data.profileDto.birthDate) {
+			data.profileDto.birthDate = new Date(data.profileDto.birthDate)
+		}
 		$scope.userDetail = data;
 	}, function error(message) {
 		toastFactory.openDialog(this, 'Hata','Kullanici detayının alınması sırasında bir hata oluştu. Hata:' + message);
